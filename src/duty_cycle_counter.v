40f3fa6fd9dbfe97fde92f3a07e3e682f409e599
PaulineKreis <Pauline.Kreis@haw-hamburg.de>
2026-06-17 00:10:58 +0200

    Fix syntax error related to braces in duty clycle counter

--- src/duty_cycle_counter.v
+++ src/duty_cycle_counter.v
@@ -1,6 +1,8 @@
 module duty_cycle_counter # (
     parameter CLK_FREQ = 25_000_000,
-    parameter WATCHDOG_TICK = CLK_FREQ,
+    parameter WATCHDOG_TICK = CLK_FREQ
+
+) (
 
     input wire i_pwm,
     input wire i_clk,
